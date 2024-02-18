8edd2e1ee0a87d74435d02b9a6321de5711b9d7a
superbono <fr.dev.ar@gmail.com>
2024-02-18 22:40:17 +0900

    issue main page list fecth

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,18 +1,53 @@
-import { Button } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
+import { Button, Table } from "@radix-ui/themes";
+import axios from "axios";
 import { Metadata } from "next";
 import Link from "next/link";
+
 export const metadata: Metadata = {
   title: "Issues Page",
   description: "이슈 페이지입니다",
 };
 
-export default function IssuesPage() {
+export default async function IssuesPage() {
+  const issues = await prisma.issue.findMany();
+
   return (
     <div>
-      <p>Issues</p>
-      <Button>
-        <Link href="/issues/new">New Issue</Link>
-      </Button>
+      <div className="mb-5">
+        <Button>
+          <Link href="/issues/new">New Issue</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              CreatedAt(생성일자)
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">{issue.status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 }
