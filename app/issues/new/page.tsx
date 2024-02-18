9855adcf66e7b2dcaeda21d1d90d75644dc1c8eb
superbono <fr.dev.ar@gmail.com>
2024-02-18 14:54:21 +0900

    simplemde - 마크다운 적용

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextField } from "@radix-ui/themes";
 import React from "react";
+import { SimpleMdeReact } from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const IssuesNewPage = () => {
   return (
@@ -9,7 +10,7 @@ const IssuesNewPage = () => {
       <TextField.Root className="mb-3">
         <TextField.Input placeholder="title" />
       </TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMdeReact placeholder="Description" />
       <Button>Create New Issue</Button>
     </div>
   );
