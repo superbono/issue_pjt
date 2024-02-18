23dce6bccb0dfabb7b4bd00e04e53ef23e281f7d
superbono <fr.dev.ar@gmail.com>
2024-02-18 19:56:39 +0900

    issue-new - 오류 구문 추가 [try catch]

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
+  title: z.string().min(1, "1글자 이상 입력해야 합니다").max(255),
   description: z.string().min(3),
 });
 
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import { Metadata } from "next";
 import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
-import React from "react";
+import React, { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+
 export const metadata: Metadata = {
   title: "New Page",
   description: "이슈 추가 페이지입니다",
@@ -23,29 +24,58 @@ interface IssueForm {
 }
 
 const IssuesNewPage = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const { register, resetField, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
   const router = useRouter();
 
+  const handleResetClick = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    resetField("title");
+    resetField("description");
+  };
+
   return (
-    <form
-      className="max-w-xl space-y-2"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root className="mb-3">
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-      <Button type="submit">Create New Issue</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+
+      <form
+        className="max-w-xl space-y-2"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("예상치 못한 오류가 발생했습니다.");
+            resetField("title");
+            resetField("description");
+          }
+        })}
+      >
+        <TextField.Root className="mb-3">
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <div className="flex justify-end space-x-2">
+          <Button
+            onClick={handleResetClick}
+            style={{ backgroundColor: "orange" }}
+          >
+            Cancle
+          </Button>
+          <Button>Create New Issue</Button>
+        </div>
+      </form>
+    </div>
   );
 };
 
