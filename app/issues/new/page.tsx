21efb6085f4a78a67261363344194da0a4b38fea
superbono <fr.dev.ar@gmail.com>
2024-02-18 17:06:40 +0900

    issue-new - crearte api 연동 및 테스트

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -5,7 +5,9 @@ import { Metadata } from "next";
 import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
 import React from "react";
-
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 export const metadata: Metadata = {
   title: "New Page",
   description: "이슈 추가 페이지입니다",
@@ -15,19 +17,35 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const IssuesNewPage = () => {
-  const handleClick = () => {
-    alert("클릭");
-  };
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
 
   return (
-    <div className="max-w-xl space-y-2">
+    <form
+      className="max-w-xl space-y-2"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
       <TextField.Root className="mb-3">
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
-      <Button onClick={handleClick}>Create New Issue</Button>
-    </div>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+      <Button type="submit">Create New Issue</Button>
+    </form>
   );
 };
 
