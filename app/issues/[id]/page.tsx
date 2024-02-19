9b8bcddf530da91f7e8f86a38e46bbb1442e8ad4
superbono <fr.dev.ar@gmail.com>
2024-02-19 10:24:53 +0900

    issue detail page style 적용

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
+import { Card, Heading, Text, Flex } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -21,10 +23,14 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex my="2" className="space-x-3 my-30" style={{ marginTop: "20px" }}>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card style={{ marginTop: "15px" }}>
+        <p>{issue.description}</p>
+      </Card>
     </div>
   );
 };
