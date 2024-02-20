0673197e528b605d1036c49d8d49b3b7a50b5ac3
superbono <fr.dev.ar@gmail.com>
2024-02-20 11:32:41 +0900

    issues detail api error 처리

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -14,46 +14,65 @@ interface Props {
 const DeleteIssueButton = ({ issueId }: Props) => {
   const [error, setError] = useState("");
   const router = useRouter();
+  const [errorStatus, setErrorStatus] = useState(false);
+
+  const onDelete = async () => {
+    try {
+      setErrorStatus(false);
+      await axios.delete(`/api/issues/${issueId}`);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      setErrorStatus(true);
+      setError("예상치 못한 오류가 발생했습니다.");
+    }
+  };
 
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">
-          <TrashIcon />
-          Delete Issue
-        </Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Delete</AlertDialog.Title>
-        <AlertDialog.Description>
-          정말 삭제하시겠습니까?
-        </AlertDialog.Description>
-        <Flex mt="4" gap="2">
-          <AlertDialog.Action>
-            <Button
-              onClick={async () => {
-                try {
-                  await axios.delete(`/api/issues/${issueId}`);
-                  router.push("/issues");
-                  router.refresh();
-                } catch (error) {
-                  setError("예상치 못한 오류가 발생했습니다.");
-                }
-              }}
-              variant="soft"
-              color="orange"
-            >
-              확인
-            </Button>
-          </AlertDialog.Action>
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              취소
-            </Button>
-          </AlertDialog.Cancel>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">
+            <TrashIcon />
+            Delete Issue
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Delete</AlertDialog.Title>
+          <AlertDialog.Description>
+            정말 삭제하시겠습니까?
+          </AlertDialog.Description>
+          <Flex mt="4" gap="2">
+            <AlertDialog.Action>
+              <Button onClick={onDelete} variant="soft" color="orange">
+                확인
+              </Button>
+            </AlertDialog.Action>
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                취소
+              </Button>
+            </AlertDialog.Cancel>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={errorStatus}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            이슈를 삭제할 수 없습니다. (오류 발생)
+          </AlertDialog.Description>
+          <Button
+            onClick={() => setErrorStatus(false)}
+            mt="4"
+            variant="soft"
+            color="gray"
+          >
+            확인
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
