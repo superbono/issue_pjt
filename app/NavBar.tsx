db530b781afadd2eada26dafd83c7f35a35c3fbe
superbono <fr.dev.ar@gmail.com>
2024-02-20 23:24:35 +0900

    navbar bar 리팩토링(모듈화)

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -20,14 +20,6 @@ import { GoTasklist } from "react-icons/go";
 */
 
 const NavBar = () => {
-  const currentPath = usePathname();
-  const { status, data: session } = useSession();
-
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
-  ];
-
   return (
     <nav className="border-b mb-5 px-5 py-3">
       <Container>
@@ -36,61 +28,79 @@ const NavBar = () => {
             <Link href="/" className="font-extrabold mr-2">
               <GoTasklist size={30} />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map((item) => (
-                <li key={item.href}>
-                  <Link
-                    className={`${
-                      item.href === currentPath
-                        ? "text-zinc-900 font-medium"
-                        : "text-zinc-500"
-                    } hover:text-zinc-800 transition-colors`}
-                    href={item.href}
-                  >
-                    {item.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              // <Link href="/api/auth/signout">Logout</Link>
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  {/* <Link href="/api/auth/signout">Logout</Link> */}
-                  {/*
-                    사용자 이미지가 존재하지 않는 상황을 대비해서 무조건 값이 할당되어있다는 표시로
-                    !를 추가하여 작성합니다.
-                  */}
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                    referrerPolicy="no-referrer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text>{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Text>
-                      <Link href="/api/auth/signout">Logout</Link>
-                    </Text>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">Login</Link>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <ul className="flex space-x-6">
+      {links.map((item) => (
+        <li key={item.href}>
+          <Link
+            className={`${
+              item.href === currentPath
+                ? "text-zinc-900 font-medium"
+                : "text-zinc-500"
+            } hover:text-zinc-800 transition-colors`}
+            href={item.href}
+          >
+            {item.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return <Link href="/api/auth/signin">Login</Link>;
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          {/*
+            사용자 이미지가 존재하지 않는 상황을 대비해서 무조건 값이 할당되어있다는 표시로
+            !를 추가하여 작성합니다.
+          */}
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text>{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Text>
+              <Link href="/api/auth/signout">Logout</Link>
+            </Text>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
