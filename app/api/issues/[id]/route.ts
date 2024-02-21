22e14474f7640780c804897a9e18357e564b9827
superbono <fr.dev.ar@gmail.com>
2024-02-21 16:18:07 +0900

    이슈 담당자 api 구현 및 코드개선

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { IssueSchema } from "@/app/utils/validationSchemas";
+import { IssueSchema, patchIssueSchema } from "@/app/utils/validationSchemas";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
@@ -8,16 +8,27 @@ export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const session = await getServerSession(AuthOptions);
-  if (!session) {
-    return NextResponse.json({}, { status: 401 });
-  }
+  // const session = await getServerSession(AuthOptions);
+  // if (!session) {
+  //   return NextResponse.json({}, { status: 401 });
+  // }
 
   const body = await request.json();
-  const validation = IssueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const { assignedToUserId, title, description } = body;
+
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+    if (!user) {
+      return NextResponse.json({ error: "Invalid User..." }, { status: 400 });
+    }
   }
 
   const issue = await prisma.issue.findUnique({
@@ -34,8 +45,9 @@ export async function PATCH(
   const updateIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId,
     },
   });
 
--- app/utils/validationSchemas.ts
+++ app/utils/validationSchemas.ts
@@ -2,5 +2,20 @@ import { z } from "zod";
 
 export const IssueSchema = z.object({
   title: z.string().min(1, "1글자 이상 입력해야합니다").max(255),
-  description: z.string().min(3, "3글자 이상 입력해야합니다."),
+  description: z.string().min(3, "3글자 이상 입력해야합니다.").max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "1글자 이상 입력해야합니다").max(255).optional(),
+  description: z
+    .string()
+    .min(3, "3글자 이상 입력해야합니다.")
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required")
+    .max(255)
+    .optional()
+    .nullable(),
 });
