e57abbe252e5ee0b15308ae2cd7d19eff726c201
superbono <fr.dev.ar@gmail.com>
2024-02-21 20:23:02 +0900

    issue main filter 배치 적용

---
+++ app/issues/list/IssueStatusFilter.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { Status } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import React from "react";
+
+const statuses: { label: string; value?: Status }[] = [
+  { label: "All" },
+  { label: "Open", value: "OPEN" },
+  { label: "InProgress", value: "IN_PROGRESS" },
+  { label: "Closed", value: "CLOSED" },
+];
+
+const IssueStatusFilter = () => {
+  return (
+    <>
+      <Select.Root>
+        <Select.Trigger placeholder="상태값으로 필터하기" />
+        <Select.Content>
+          {statuses.map((status) => (
+            <Select.Item key={status.value} value={status.value || "ALL"}>
+              {status.label}
+            </Select.Item>
+          ))}
+        </Select.Content>
+      </Select.Root>
+    </>
+  );
+};
+
+export default IssueStatusFilter;
