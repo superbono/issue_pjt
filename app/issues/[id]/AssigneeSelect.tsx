d80a992cd40f2c33323f1dfba9e37e36c0d92c8f
superbono <fr.dev.ar@gmail.com>
2024-02-21 15:10:03 +0900

    react-query를 이용한 데이터 패칭

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,20 +1,39 @@
 "use client";
+import { Skeleton } from "@/app/components/Skeleton";
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  // const [users, setUsers] = useState<User[]>([]);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
+  // useQuery 함수는 isLoading, error, data 반환
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    // 이전에 가져온 값을 갱신하려면 시간을 설정해야 서버에서 체크 가능
+    staleTime: 60 * 1000,
+    // 갱신된 데이터를 가져오는 것을 시도할 횟수 (실패할 수 있어서 체크)
+    retry: 3,
+  });
 
-    fetchUsers();
-  }, []);
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     const { data } = await axios.get<User[]>("/api/users");
+  //     setUsers(data);
+  //   };
+
+  //   fetchUsers();
+  // }, []);
+
+  if (isLoading) return <Skeleton />;
+  if (error) return null;
 
   return (
     <Select.Root>
@@ -22,7 +41,7 @@ const AssigneeSelect = () => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item value={user.id} key={user.id}>
               {user.name}
             </Select.Item>
